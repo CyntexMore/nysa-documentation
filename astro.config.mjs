b76b1dc820373f7cedee2822e130a3b9a09791b0
Dávid Szekeres <saynedbread@synthspace.xyz>
2026-01-26 21:59:31 +0100

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,41 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightThemeNext from "starlight-theme-next";
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      plugins: [starlightThemeNext()],
+      title: "Nysa Documentation",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/CyntexMore/nysa",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Introduction",
+          items: [
+            { label: "What is this?", slug: "introduction/what-is-this" },
+            { label: "Setup", slug: "introduction/setup" },
+          ],
+        },
+        {
+          label: "Specifications",
+          collapsed: true,
+          items: [
+            { label: "Architecture", slug: "specifications/architecture" },
+            {
+              label: "Memory Engine (nmgine)",
+              slug: "specifications/memory-engine",
+            },
+          ],
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+    }),
+  ],
 });
